5dc0e8345ce4a078dd083bb8ff7a7b6a1deb7f8c
mr-alloc <mr.allocater@gmail.com>
2024-03-17 22:32:34 +0900

    메인 달력뷰 스타일링

--- src/router/index.ts
+++ src/router/index.ts
@@ -27,7 +27,7 @@ router.beforeEach(async (to, from, next) => {
 
     const onlyForGuest = ['/sign-in', '/sign-up'];
 
-    console.log(`[${to.path}] [No Session: ${noAccessToken()} / No Member: ${memberInfoStore.needMemberInfo()}]`)
+    // console.log(`[${to.path}] [No Session: ${noAccessToken()} / No Member: ${memberInfoStore.needMemberInfo()}]`)
 
     //로그인 정보가 없는 경우
     if (noAccessToken() && to.meta.role !== 0) {
@@ -49,8 +49,6 @@ router.beforeEach(async (to, from, next) => {
 
     //로그인은 했지만, 멤버 정보가 없는경우.
     if (!noAccessToken() && memberInfoStore.needMemberInfo()) {
-        console.log('need member info. (renew member info)')
-
         await call(MemberAPI.GetInfo, null,
             (response) => {
                 const { id, nickname, role } = response.data
@@ -61,7 +59,6 @@ router.beforeEach(async (to, from, next) => {
                     notificationStore.notice(NotificationType.NONE, "반가워요!", `${nickname}님, 오늘도 좋은 하루 되세요!`)
                 }
                 memberInfoStore.updateMemberInfo(new MemberInfo(id, nickname, role))
-                console.log('Successfully renew member profile.')
                 return;
             },
             (sepc, error) => {
@@ -80,14 +77,12 @@ router.beforeEach(async (to, from, next) => {
         const authorityRole: number = memberInfo.role
 
         const { role } = to.meta as { role: number }
-        console.info(`${to.path}: Need: ${role} / Current: ${authorityRole}`)
         if (role && role > authorityRole) {
             notificationStore.notice(NotificationType.WARNING, "부적절한 접근 경고", "잘못된 방법으로 접근이 감지 되었습니다. 지속적으로 올바르지 않은 접근시 이용에 제한이 될 수 있습니다.", 10)
             return next({ path: '/' })
         }
     }
 
-    console.log('no reachable to any condition')
     return next()
 })
 
--- src/views/MemberCalendar.vue
+++ src/views/MemberCalendar.vue
@@ -1,50 +1,88 @@
 <template>
   <div class="calendar-container" v-show="state.thisMonth">
     <div class="calendar-title">
-      <span class="title-text">{{ methods.getCalendarTitle() }}</span>
+      <span class="title-text">{{ state.calendarTitle }}</span>
+      <div class="calendar-control">
+        <button class="control-button" type="button" v-on:click="methods.setMonth(-1)">
+          <FontAwesomeIcon icon="caret-left"/>
+        </button>
+        <button class="control-button" type="button" v-on:click="methods.setMonth(+1)">
+          <FontAwesomeIcon icon="caret-right"/>
+        </button>
+      </div>
     </div>
-    <ul class="calendar-row calendar-header">
-      <li>일</li>
-      <li>월</li>
-      <li>화</li>
-      <li>수</li>
-      <li>목</li>
-      <li>금</li>
-      <li>토</li>
-    </ul>
-    <ul class="calendar-row member-calender">
-      <li class="each-day-item">
-        <div class="item-header">
-        </div>
-        <div class="item-body">
-        </div>
-      </li>
-    </ul>
+    <div class="calendar-wrapper">
+      <ul class="calendar-row calendar-header">
+        <li>일</li>
+        <li>월</li>
+        <li>화</li>
+        <li>수</li>
+        <li>목</li>
+        <li>금</li>
+        <li>토</li>
+      </ul>
+      <Transition name="fade">
+        <ul class="calendar-row member-calendar" v-show="state.calendar">
+          <li class="each-day-item" v-for="(date, index) in state.calendar" :key="index">
+            <div class="item-header">
+              {{ date.get('date') == 1 ? date.format('M월 D일') : date.format('D일') }}
+            </div>
+            <div class="item-body">
+              <ul class="daily-schedules">
+                <li class="each-schedule">
+                  <span>심심한 일정</span>
+                </li>
+              </ul>
+              <div class="more-button">
+                <span>+9</span>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </Transition>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import {onMounted, reactive} from "vue";
+import moment, {type Moment} from "moment";
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 
 const state = reactive({
+  calendarTitle: '',
   thisMonth: new Date(),
-  calendar: []
+  calendar: [] as Array<Moment>,
+  startDate: moment(),
+  endDate: moment()
 })
 const methods = {
-  getCalendarTitle() {
-    return state.thisMonth.getFullYear() + "년 " + (state.thisMonth.getMonth() + 1) + "월";
+  setMonth(month: number) {
+    state.thisMonth.setMonth(state.thisMonth.getMonth() + month);
+    methods.drawCalendar();
+  },
+  drawCalendar() {
+    state.calendarTitle = state.thisMonth.getFullYear() + "년 " + (state.thisMonth.getMonth() + 1) + "월"
+    const startOfThisMonth = moment(state.thisMonth).startOf('month');
+    const startOfCalendar = startOfThisMonth.subtract(startOfThisMonth.day(), 'days');
+    state.startDate = startOfCalendar.clone();
+    // print start of this month's day and days
+
+    const endOfThisMonth = moment(state.thisMonth).endOf('month');
+    const endOfCalendar = endOfThisMonth.add(7 - endOfThisMonth.day(), 'days');
+    state.endDate = endOfCalendar.clone();
+    let interval = 0;
+    state.calendar = [];
+    while (interval < endOfCalendar.diff(startOfCalendar, 'days')) {
+      const cloned = startOfCalendar.clone();
+      const date = cloned.add(interval, 'days');
+      state.calendar.push(date);
+      interval++;
+    }
   }
 }
 onMounted(() => {
-  const today = new Date();
-  const firstDay = new Date(today.getFullYear(), today.getMonth(), 1);
-  const lastDay = new Date(today.getFullYear(), today.getMonth() + 1, 0);
-
-  const firstDayOfWeek = firstDay.getDay();
-  const lastDayOfWeek = lastDay.getDay();
-
-
-
+  methods.drawCalendar()
 })
 </script>
 
@@ -55,51 +93,168 @@ onMounted(() => {
   height: 100%;
   min-width: 648px;
   margin: 0 auto;
+  display: flex;
+  flex-direction: column;
 
   .calendar-title {
-    margin: 10px 0 0px;
+    margin: 10px 0 0;
+    height: 60px;
+    flex-shrink: 0;
+    padding: 0 20px;
+    display: flex;
+    flex-direction: row;
 
     .title-text {
       font-size: 2rem;
       font-weight: bold;
+      flex-shrink: 0;
+    }
 
+    .calendar-control {
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-end;
+      margin-top: 10px;
+      flex-grow: 1;
+      align-items: center;
+
+      .control-button {
+        margin-left: 10px;
+        padding: 3px 10px;
+        border-radius: 5px;
+        transition: $duration;
+        cursor: pointer;
+        height: 30px;
+
+        &:hover {
+          background-color: rgb(0, 0, 0, .2);
+        }
+      }
     }
   }
 
-  .calendar-row {
-    padding: 0;
+  .calendar-wrapper {
     display: flex;
-    flex-direction: row;
-    width: 100%;
-    list-style: none;
+    flex-direction: column;
+    flex-grow: 1;
 
-    li {
-      padding: 3px;
-      flex: 1 1 14%;
-      text-align: center;
+
+    .calendar-row {
+      padding: 0;
+      display: flex;
+      flex-direction: row;
+      width: 100%;
+      list-style: none;
+
+      li {
+        padding: 3px;
+        flex: 1 1 14%;
+        text-align: center;
+      }
     }
-  }
 
-  .calendar-header {
-    border-top: 1px solid $standard-light-gray-in-white;
-    border-bottom: 1px solid $standard-light-gray-in-white;
+    .calendar-header {
+      border-top: 1px solid $standard-light-gray-in-white;
+      border-bottom: 1px solid $standard-light-gray-in-white;
+      height: 30px;
+      flex-shrink: 0;
 
-    li:nth-child(1) {
-      background-color: #f8dddd;
+      li {
+        font-weight: bold;
+      }
+
+      li:nth-child(1) {
+        background-color: #f8dddd;
+      }
+
+      li:nth-child(7) {
+        background-color: #d1dbee;
+      }
+
+      li:not(:last-child) {
+        border-right: 1px solid $standard-light-gray-in-white;
+      }
     }
 
-    li:nth-child(7) {
-      background-color: #d1dbee;
-    }
-  }
+    .member-calendar {
+      display: flex;
+      flex-direction: row;
+      flex-wrap: wrap;
+      flex-grow: 1;
 
-  .member-calender {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
+      .each-day-item {
+        border-right: 1px solid $standard-light-gray-in-white;
+        border-bottom: 1px solid $standard-light-gray-in-white;
+        transition: $duration;
+        cursor: pointer;
+        display: flex;
+        flex-direction: column;
 
-    li {
 
+        .item-header {
+          text-align: left;
+          font-weight: bold;
+          font-size: .64rem;
+          user-select: none;
+          height: 15px;
+          flex-shrink: 0;
+        }
+
+        .item-body {
+          flex-grow: 1;
+
+          display: flex;
+          flex-direction: column;
+
+          .daily-schedules {
+            list-style: none;
+            padding: 0;
+            flex-grow: 1;
+
+            .each-schedule {
+              width: 100%;
+              border-radius: 5px;
+              margin: 3px 0;
+              text-align: left;
+              padding: 0 3px;
+              flex-grow: 1;
+              font-size: .74rem;
+              transition: $duration;
+
+              &:hover {
+                background-color: rgb(0, 0, 0, .2);
+              }
+            }
+
+          }
+
+
+          .more-button {
+            font-size: .64rem;
+            display: flex;
+            flex-direction: row;
+            justify-content: flex-end;
+            height: 15px;
+            flex-shrink: 0;
+            padding: 0 3px;
+            transition: $duration;
+            border-radius: 5px;
+
+            span {
+              font-weight: bold;
+            }
+
+
+            &:hover {
+              background-color: rgb(0, 0, 0, .2);
+            }
+          }
+        }
+
+        &:hover {
+          //background-color: rgb(0, 0, 0, .2)
+        }
+      }
     }
   }
 }
